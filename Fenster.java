8b9f166f92a7394d180c4fa3d32777e8a1c704ea
Simon Schneider <schneider_berghausen@web.de>
2023-05-23 09:07:22 +0200

    ...

--- Fenster.java
+++ Fenster.java
@@ -11,8 +11,15 @@ public class Fenster extends JFrame implements ActionListener
 {
     // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
     private JLabel labelPunkte;
+
     private JLabel dinoBild;
     URL kaktus_url = this.getClass().getResource("kaktus.png");
+
+    private JLabel labelAnmelden;
+    private JTextField bn;
+    private JTextField pw;
+    private JButton buttonweiter;
+
     
     /**
      * Konstruktor für Objekte der Klasse Spielfeld
@@ -38,8 +45,48 @@ public class Fenster extends JFrame implements ActionListener
         super.add(labelPunkte);
         super.add(dinoBild);
         
-        SpielfeldAufbauen();
+        //---------------------Anmelden------------------------
+        labelAnmelden = new JLabel();
+        labelAnmelden.setText("Anmelden: ");
+        labelAnmelden.setLocation(50,150);
+        labelAnmelden.setSize(300,50);
+        labelAnmelden.setFont(labelAnmelden.getFont().deriveFont(46f));
         
+        //Benutzernameeingabefeld
+        bn = new JTextField();
+        bn.setText("Benutzername");
+        bn.setLocation(280,250);
+        bn.setSize (550,80);
+        bn.setEnabled(true);
+        bn.setFont(bn.getFont().deriveFont(56f));
+        
+        //Passworteingabefeld
+        pw = new JTextField();
+        pw.setText("Passwort");
+        pw.setLocation(280,330);
+        pw.setSize (550,80);
+        pw.setEnabled(true);
+        pw.setFont(pw.getFont().deriveFont(56f));
+       
+        //Knopf
+        buttonweiter = new JButton();
+        buttonweiter.setText("Fortfahren");
+        buttonweiter.setLocation(320, 450);
+        buttonweiter.setSize (470, 50);
+        buttonweiter.setEnabled(true);
+        buttonweiter.setFont(buttonweiter.getFont().deriveFont(56f));
+        buttonweiter.addActionListener(this);
+        
+        // Anmelden, Benutzernameeingabefeld,Passworteingabefeld, Knopf hinzufügen
+        super.add(labelAnmelden);
+        super.add(bn);
+        super.add(pw);
+        super.add(buttonweiter);
+        
+       // SpielfeldAufbauen();
+        AnmeldenAufbauen();
+        
+        buttonweiter.addActionListener(this);
         // Fenster konfigurieren
         super.setLayout(null);
         super.setSize(1300,750);
@@ -52,6 +99,12 @@ public class Fenster extends JFrame implements ActionListener
     public void SpielfeldAufbauen(){
         // Punkte sichtbar
         labelPunkte.setVisible(true);
+        labelAnmelden.setVisible(false);
+    }
+    public void AnmeldenAufbauen(){
+        // Anmelden sichtbar
+        labelAnmelden.setVisible(true);
+        labelPunkte.setVisible(false);
     }
     
     /**
